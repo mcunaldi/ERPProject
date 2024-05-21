7c739166e3939518b77f265add1c82e439fcb3d3
Mehmet Can ÜNALDI <mehmetcanunaldi@gmail.com>
2024-05-21 06:08:48 +0300

    B4, 12. Completed. Added pipe.

--- ERPClient/src/app/components/home/home.component.ts
+++ ERPClient/src/app/components/home/home.component.ts
@@ -8,6 +8,4 @@ import { SharedModule } from '../../modules/shared.module';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
