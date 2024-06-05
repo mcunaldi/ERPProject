92364c982887ae7ae25d01769fe3aee29dad4503
Mehmet Can ÜNALDI <mehmetcanunaldi@gmail.com>
2024-06-06 00:11:23 +0300

    Commit & push

--- ERPServer/ERPServer.Application/obj/Debug/net8.0/ERPServer.Application.AssemblyInfo.cs
+++ ERPServer/ERPServer.Application/obj/Debug/net8.0/ERPServer.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7533dda4786de992f13940c98b909714668d3994")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66f07350eac106fa9edfe3a7e6a5fd9d0fa9f95d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPServer.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
